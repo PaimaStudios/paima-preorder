c819b4a2167aa634f1e640129b80f52ca3cebaa3
Matej Poklemba <matejpoklemba@gmail.com>
2024-01-17 15:52:57 +0100

    Update deploy script and readme

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -15,16 +15,18 @@ contract Deploy is Script {
         // Define sale price in native gas tokens
         uint256 nftNativePrice = 0.001 ether;
         // Define sale price in supported ERC20 tokens
-        uint256 nftErc20Price = 1 * 1e6;
+        uint256 nftErc20Price = 100 * 1e6;
+        // Define supported ERC20 payment tokens
+        IERC20[] memory supportedCurrencies;
+        supportedCurrencies[0] = IERC20(0xF200edFf719b0519eb3ced3cc05802D493A04ca8); // XAI testnet USDC
         // Define NFT name
         string memory nftName = "Tarochi Season 1 Pass";
         // Define NFT symbol
-        string memory nftSymbol = "TAR1";
-        // Define NFT max supply
+        string memory nftSymbol = "TSP1";
+        // Define NFT max supply (unlimited)
         uint256 nftMaxSupply = type(uint256).max;
         // Define sale deadline timestamp
-        // Thu Feb 15 2024 00:00:00 GMT+0000
-        uint256 mintDeadline = 1707955200;
+        uint256 mintDeadline = 1707955200; // Thu Feb 15 2024 00:00:00 GMT+0000
 
         address ownerAddress = vm.envAddress("CONTRACT_OWNER_ADDRESS");
         string memory nftUri = vm.envString("NFT_URI");
@@ -43,6 +45,8 @@ contract Deploy is Script {
             "initialize(address,address,uint256,uint256)", ownerAddress, address(nft), nftNativePrice, nftErc20Price
         );
         ERC1967Proxy tarochiSaleProxy = new ERC1967Proxy{salt: dummySalt}(address(tarochiSaleImpl), initializeData);
+
+        TarochiSale(address(tarochiSaleProxy)).whitelistTokens(supportedCurrencies);
 
         console2.log("Tarochi NFT address:", address(nft));
         console2.log("Tarochi Sale implementation:", address(tarochiSaleImpl));
