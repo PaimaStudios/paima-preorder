dc13dd9517a1c28a4eb4c2811c3b4a592b092866
Matej Poklemba <matejpoklemba@gmail.com>
2024-08-12 16:11:10 +0200

    Fix refunds endpoint not lowercasing wallet

--- backend/api/src/controllers/refunds.ts
+++ backend/api/src/controllers/refunds.ts
@@ -17,7 +17,10 @@ export class RefundsController extends Controller {
     const pool = requirePool();
     const launchpadAddress = launchpadsData.find(lpad => lpad.slug === launchpad)?.address;
     const stats = launchpadAddress
-      ? await getRefunds.run({ launchpad: launchpadAddress, wallet: wallet ?? null }, pool)
+      ? await getRefunds.run(
+          { launchpad: launchpadAddress, wallet: wallet?.toLowerCase() ?? null },
+          pool
+        )
       : [];
     return { stats };
   }
